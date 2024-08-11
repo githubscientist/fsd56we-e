3b8f0da9cdb9b58fac4b5ccde571f31882ddf1a7
githubscientist <sathish.datascientist@gmail.com>
2024-08-11 16:01:25 +0530

    added endpoint --creating a new todo

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 // import the express module
 const express = require('express');
 
+const Todo = require('./models/todo');
+
 // create an express application
 const app = express();
 
 // use the express middleware for parsing json data
 app.use(express.json());
 
-app.get("/", (request, response) => {
-    response.json({ message: "Hello, World!!" });
-});
+app.post('/api/v1/todos', async (req, res) => {
+    try {
+        // get the description from the request body
+        const { description } = req.body;
+
+        // create a new todo
+        const newTodo = new Todo({
+            description
+        });
+
+        // save the todo to the database
+        const savedTodo = await newTodo.save();
+
+        // send the saved todo as a response
+        res.send({ message: 'Todo created successfully', todo: savedTodo });
+    } catch (error) {
+        res.status(500).send({ message: error.message });
+    }
+})
 
 module.exports = app;
---
+++ models/todo.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+// create a schema
+const todoSchema = new mongoose.Schema({
+    description: String,
+    status: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+// create a model and export it
+module.exports = mongoose.model('Todo', todoSchema, 'todos');

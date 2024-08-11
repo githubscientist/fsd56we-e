8bfca24ac18409ddabf1eb8498d22f991f2ead19
githubscientist <sathish.datascientist@gmail.com>
2024-08-11 15:18:36 +0530

    start server after db connection

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const { MONGODB_URI, PORT } = require('./utils/config');
 
 const mongoose = require('mongoose');
 
-// start the server and listen on port 3001
-app.listen(PORT, () => {
-    console.log(`Server running on port 3001 at http://127.0.0.1:${PORT}`);
-});
-
 mongoose.connect(MONGODB_URI)
     .then(() => {
         console.log('Connected to MongoDB');
+
+        // start the server and listen on port 3001
+        app.listen(PORT, () => {
+            console.log(`Server running on port 3001 at http://127.0.0.1:${PORT}`);
+        });
     })
     .catch((error) => {
         console.error('Error connecting to MongoDB: ', error);

fd2540b709b2db7e1ce81feaa1c15fa1b631f7a8
githubscientist <sathish.datascientist@gmail.com>
2024-08-11 15:15:25 +0530

    moving dotenv config to utils

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const app = require('./app');
+const { MONGODB_URI, PORT } = require('./utils/config');
 
 const mongoose = require('mongoose');
 
-require('dotenv').config();
-
 // start the server and listen on port 3001
-app.listen(3001, () => {
-    console.log(`Server running on port 3001 at http://127.0.0.1:3001`);
+app.listen(PORT, () => {
+    console.log(`Server running on port 3001 at http://127.0.0.1:${PORT}`);
 });
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(MONGODB_URI)
     .then(() => {
         console.log('Connected to MongoDB');
     })

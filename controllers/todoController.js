6e820590475dc39ca3da8e1aeb62722af97b88f9
githubscientist <sathish.datascientist@gmail.com>
2024-08-17 10:53:39 +0530

    model, routes, controllers --logic separated

---
+++ controllers/todoController.js
@@ -0,0 +1,25 @@
+const Todo = require('../models/todo');
+
+const todoController = {
+    createTodo: async (req, res) => {
+        try {
+            // get the description from the request body
+            const { description } = req.body;
+
+            // create a new todo
+            const newTodo = new Todo({
+                description
+            });
+
+            // save the todo to the database
+            const savedTodo = await newTodo.save();
+
+            // send the saved todo as a response
+            res.send({ message: 'Todo created successfully', todo: savedTodo });
+        } catch (error) {
+            res.status(500).send({ message: error.message });
+        }
+    }
+}
+
+module.exports = todoController;
